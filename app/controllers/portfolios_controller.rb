ca1c47ff99addee8e4bfc600e67085bb37eeaa3d
8kiran8bisht <8kiran8bisht@gmail.com>
2020-05-30 22:42:30 -0400

    created edit functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -10,15 +10,31 @@ class PortfoliosController < ApplicationController
 	def create
     @portfolio_items = Portfolio.new( params.require(:portfolio).permit(:title,:subtitle,:body))
 
-    respond_to do |format|
-      if @portfolio_items.save
-        format.html { redirect_to portfolios_path, notice: 'weldone.' }
-        
-      else
-        format.html { render :new }
-       end
+    	respond_to do |format|
+	      if @portfolio_items.save
+	        format.html { redirect_to portfolios_path, notice: 'weldone.' }
+	        
+	      else
+	        format.html { render :new }
+	       end
+    	end
+  	end
+
+  	def edit
+  		@portfolio_items = Portfolio.find(params[:id])
+  	end
+
+  	 def update
+  	 	respond_to do |format|
+  	 	@portfolio_items = Portfolio.find(params[:id])
+     	 if @portfolio_items.update(params.require(:portfolio).permit(:title,:subtitle,:body))
+			format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        else
+        	format.html { render :edit }
+        end
     end
-  end
+    end
+
 
    
 end
