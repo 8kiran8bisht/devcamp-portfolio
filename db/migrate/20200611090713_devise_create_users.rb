4cc69d1b5c1d6af9b6536fcaab59a6d8f5337b0f
8kiran8bisht <8kiran8bisht@gmail.com>
2020-06-24 04:32:00 -0400

    login functionality

--- db/migrate/20200611090713_devise_create_users.rb
+++ db/migrate/20200611090713_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
-      ## Custom filelds
+      ## Custom fields
       t.string :name
 
       ## Recoverable
