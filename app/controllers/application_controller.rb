d430018fb06ccf55650e0b434be190f9d1be3fb8
8kiran8bisht <8kiran8bisht@gmail.com>
2020-06-28 20:24:42 -0400

    layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
 include DeviseWhitelist
 include SetSource
+include DefaultPageContent
 
+	def current_user
+		super || OpenStruct.new(name: "Guste Bisht", first_name: "Guste", last_name:"Bisht",email:"test@test.com")	
+	end 
 end
